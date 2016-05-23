ede25afb1894e0e25f66a9775bd59d86fc8f3e48
dimdimas <dimas.abdurrahmans@gmail.com>
2016-05-23 13:42:16 +0700

    give comment on app.js for api url

--- public/app/app.js
+++ public/app/app.js
@@ -1,2 +1,7 @@
 var app = angular.module('employeeRecords', [])
-        .constant('API_URL', 'http://localhost/angulara/public/api/v1/');
+        .constant('API_URL', 'http://learn.pls/angulara/public/api/v1/');
+
+      /* change
+        .constant('API_URL', 'http://{{your local address/your hosting}}/angulara/public/api/v1/');
+
+      */
